9c7284c01fdef22efe772aeb607df32a4f1ab37c
danhillida <dan.hill.ida@gmail.com>
2022-03-23 10:59:21 -0600

    Added confirmation prompts

--- JS todo List/app.js
+++ JS todo List/app.js
@@ -8,9 +8,10 @@ let launcher = NaN;
 
 while (launcher !== "quit") {
   launcher = prompt("What would you like to do?");
-  console.log(launcher);
   if (launcher === "new") {
-    list.push(prompt("What would you like to add?"));
+    let add = prompt("What would you like to add?");
+    list.push(add);
+    console.log(`${add} was added to the list.`);
   } else if (launcher === "list") {
     console.log("*************");
     for (let i = 0; i < list.length; i++) {
@@ -21,6 +22,11 @@ while (launcher !== "quit") {
     let remove = prompt(
       "What is the index of the item you would like to remove?"
     );
+    console.log(`${list[remove]} was deleted from list.`);
     list.splice(remove, 1);
+  } else {
+    console.log("I don't recognize that.");
   }
 }
+
+console.log("You have quit the app");
